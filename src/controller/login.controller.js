f478cd41d913e980a0834d01d73859421dd1eef8
medeepakmishra <146239205+medeepakmishra@users.noreply.github.com>
2026-01-21 11:41:20 +0530

    Login without OTP complete

---
+++ src/controller/login.controller.js
@@ -0,0 +1,53 @@
+// import User from "../models/User.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required"
+      });
+    }
+
+    const emailNormalized = email.toLowerCase();
+    const user = await User.findOne({ email: emailNormalized }).select("+password");
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid email or password"
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({
+        message: "Invalid email or password"
+      });
+    }
+
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+        number:user.number
+      }
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error form login controller" });
+  }
+};
+ 
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,16 @@
 import express from "express";
 import { registerUser } from "../controller/auth.controller.js";
+import { loginUser } from "../controller/login.controller.js";
 
 const router = express.Router();
 
 // registration route
 router.post("/register", registerUser);
+router.post("/login", loginUser);
 
 export default router;
+
+
+
+
+
