4e3241c2f88cfc67dda30798f28c2a7e2be880f9
medeepakmishra <146239205+medeepakmishra@users.noreply.github.com>
2026-07-13 08:50:42 +0530

    change in upload controller

--- src/controller/upload.controller.js
+++ src/controller/upload.controller.js
@@ -49,31 +49,151 @@
 // };
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+// import StudentProfile from "../models/studentProfile.model.js";
+
+
+// export const uploadResume = async (req,res)=>{
+
+//     try {
+
+
+//         console.log("USER:", req.user);
+
+//         console.log("FILE:", req.file);
+
+
+
+//         if(!req.file){
+
+//             return res.status(400).json({
+
+//                 success:false,
+//                 message:"Please upload resume"
+
+//             });
+
+//         }
+
+
+
+//         const profile = await StudentProfile.findOne({
+
+//             user:req.user._id
+
+//         });
+
+
+
+//         if(!profile){
+
+//             return res.status(404).json({
+
+//                 success:false,
+//                 message:"Student profile not found"
+
+//             });
+
+//         }
+
+
+
+//         // Cloudinary data from multer
+
+//         profile.resume = {
+
+//             url:req.file.path,
+
+//             publicId:req.file.filename
+
+//         };
+
+
+
+//         await profile.save();
+
+
+
+//         return res.status(200).json({
+
+//             success:true,
+
+//             message:"Resume uploaded successfully",
+
+//             resume:profile.resume
+
+//         });
+
+
+
+//     }
+//     catch(error){
+
+//         console.error(
+//             "Resume Upload Error:",
+//             error
+//         );
+
+
+//         return res.status(500).json({
+
+//             success:false,
+
+//             message:"Resume upload failed"
+
+//         });
+
+//     }
+
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import StudentProfile from "../models/studentProfile.model.js";
+import cloudinary from "../config/cloudinary.js";
 
 
-export const uploadResume = async (req,res)=>{
+export const uploadResume = async (req, res) => {
 
     try {
 
-
         console.log("USER:", req.user);
-
         console.log("FILE:", req.file);
 
 
-
-        if(!req.file){
+        if (!req.file) {
 
             return res.status(400).json({
-
                 success:false,
                 message:"Please upload resume"
-
             });
 
         }
-
 
 
         const profile = await StudentProfile.findOne({
@@ -83,8 +203,7 @@ export const uploadResume = async (req,res)=>{
         });
 
 
-
-        if(!profile){
+        if (!profile) {
 
             return res.status(404).json({
 
@@ -97,11 +216,21 @@ export const uploadResume = async (req,res)=>{
 
 
 
-        // Cloudinary data from multer
+        // Create browser-friendly PDF URL
+
+        const pdfUrl = cloudinary.url(
+            req.file.filename,
+            {
+                resource_type:"raw",
+                secure:true
+            }
+        );
+
+
 
         profile.resume = {
 
-            url:req.file.path,
+            url: pdfUrl,
 
             publicId:req.file.filename
 
@@ -138,7 +267,7 @@ export const uploadResume = async (req,res)=>{
 
             success:false,
 
-            message:"Resume upload failed"
+            message:error.message
 
         });
 
